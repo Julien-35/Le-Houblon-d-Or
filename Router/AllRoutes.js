dde3c8c59843440f110d633ab07611f334c10d13
julien-35 <julien45.dubois@gmail.com>
2024-07-25 09:46:05 +0200

    Ajouter de plusieurs pages HTML et redirection de la page accueil

--- Router/AllRoutes.js
+++ Router/AllRoutes.js
@@ -2,7 +2,15 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "home.html"),];
+    new Route("/", "Accueil", "pages/home.html"),
+    new Route("/biere", "Accueil", "pages/biere.html"),
+    new Route("/evenement", "Accueil", "pages/evenement.html"),
+    new Route("/contact", "Accueil", "pages/contact.html"),
+    new Route("/admin", "Accueil", "pages/admin.html"),
+    new Route("/employe", "Accueil", "pages/employe.html"),
+    new Route("/signin", "Accueil", "pages/signin.html"),];
+
+
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Le Houblon d'Or";
