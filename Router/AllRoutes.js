786f7812009cf71a2f358e1ae9ce88d99570899b
julien-35 <julien45.dubois@gmail.com>
2024-07-29 17:50:34 +0200

    ajout page service et ajout des pages JS dans allRoute.js

--- Router/AllRoutes.js
+++ Router/AllRoutes.js
@@ -3,10 +3,11 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "pages/home.html", [] , "js/home.js"),
-    new Route("/biere", "Accueil", "pages/biere.html"),
-    new Route("/evenement", "Accueil", "pages/evenement.html"),
-    new Route("/contact", "Accueil", "pages/contact.html"),
-    new Route("/admin", "Accueil", "pages/admin.html"),
+    new Route("/biere", "Accueil", "pages/biere.html", [] , ""),
+    new Route("/service", "Accueil", "pages/service.html", [] , ""),
+    new Route("/evenement", "Accueil", "pages/evenement.html", [] , ""),
+    new Route("/contact", "Accueil", "pages/contact.html", [] , ""),
+    new Route("/admin", "Accueil", "pages/admin.html", [] , ""),
     new Route("/employe", "Accueil", "pages/employe.html", [] , "js/employe.js"),
     new Route("/signin", "Accueil", "pages/signin.html"),];
 
