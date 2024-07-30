539b376be8b0da324ba2d254fdba0348b9e5829f
julien-35 <julien45.dubois@gmail.com>
2024-07-30 17:11:24 +0200

    Création formulaire + envoi avec MailTrap et symfony/mailer

--- Router/AllRoutes.js
+++ Router/AllRoutes.js
@@ -6,7 +6,7 @@ export const allRoutes = [
     new Route("/biere", "Accueil", "pages/biere.html", [] , ""),
     new Route("/service", "Accueil", "pages/service.html", [] , "js/service.js"),
     new Route("/evenement", "Accueil", "pages/evenement.html", [] , "js/evenement.js"),
-    new Route("/contact", "Accueil", "pages/contact.html", [] , ""),
+    new Route("/contact", "Accueil", "pages/contact.html", [] , "js/contact.js"),
     new Route("/admin", "Accueil", "pages/admin.html", [] , ""),
     new Route("/employe", "Accueil", "pages/employe.html", [] , "js/employe.js"),
     new Route("/signin", "Accueil", "pages/signin.html"),];
---
+++ js/contact.js
@@ -0,0 +1,34 @@
+
+document.getElementById('submitContactForm').addEventListener('click', async () => {
+    const email = document.getElementById('email').value;
+    const reason = document.getElementById('raison').value;
+    const message = document.getElementById('message').value;
+
+    const formData = {
+        email: email,
+        reason: reason,
+        message: message
+    };
+
+    try {
+        const response = await fetch('http://localhost:8000/api/contact/post', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(formData)
+        });
+
+        if (response.ok) {
+            // Afficher le modal en cas de succès
+            const modal = new bootstrap.Modal(document.getElementById('ModalContact'));
+            modal.show();
+            document.getElementById('contactForm').reset();
+        } else {
+            alert('Une erreur est survenue. Veuillez réessayer.');
+        }
+    } catch (error) {
+        console.error('Erreur:', error);
+        alert('Une erreur est survenue. Veuillez réessayer.');
+    }
+});
