69dd3ab7d269cdfa9f8aadd3b9c366cffa38cfb6
julien-35 <julien45.dubois@gmail.com>
2024-08-07 13:45:24 +0200

    MAJ événements sur la page d'accueil

--- js/home.js
+++ js/home.js
@@ -86,3 +86,49 @@ document.getElementById('avisForm').addEventListener('submit', async function(ev
         alert("Impossible d'envoyer l'avis. Veuillez réessayer plus tard.");
     }
 });
+
+
+
+const evenement = document.getElementById("GetEvenement");
+
+if (document.readyState === "loading") {
+    // Loading hasn't finished yet
+    evenement.addEventListener('DOMContentLoaded', voirEvenement);
+  } else {
+    voirEvenement();
+  }
+
+document.addEventListener('DOMContentLoaded', voirEvenement);
+
+async function voirEvenement() {
+    const myHeaders = new Headers();
+    myHeaders.append("X-AUTH-TOKEN", "38f1c426526d1aeebb80d777b8733f1ef09fc484");
+    myHeaders.append("Content-Type", "application/json");
+  
+    const requestOptions = {
+        method: "GET",
+        headers: myHeaders,
+        redirect: "follow",
+    };
+  
+    try {
+        const response = await fetch("https://127.0.0.1:8000/api/evenement/get", requestOptions);
+        if (!response.ok) {
+            throw new Error('Echec concernant le Fetch de evenement');
+        }
+        const result = await response.json();
+  
+        let content = '';
+        result.forEach(item => {
+                content += `                            
+                    <div class="d-flex justify-content-normal align-items-baseline"> 
+                        <h3 class="pe-5">-    ${item.nom} :</h3>
+                        <p>${item.description}</p>
+                    </div> `;
+        });     
+        document.getElementById("GetEvenement").innerHTML = content;
+    } catch (error) {
+        console.error('Error:', error);
+        console.log("Impossible de récupérer les informations des evenements");
+    }
+}
